32c1376ff81b5360f32c6b2ad653e03927cdfc58
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-07-26 14:29:24 +0300

    new changes

--- rubly/management_system/views.py
+++ rubly/management_system/views.py
@@ -1,20 +1,13 @@
-
-
 from django.shortcuts import redirect, render
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
-
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.views import View
 import json
 from django.contrib.auth.models import User
 from django.http import JsonResponse
-
-
-
-
 from django.contrib.auth.decorators import login_required
 
 
@@ -35,25 +28,18 @@ def Login(request):
         return render(request, 'auth-login-basic.html')
 
 
-
-
 # Dashboard Module
 @login_required(login_url="/login/")
 def Dashboard(request):
-    
-    if 'search' in request.GET:
-        id = request.GET['search']
-        obj = SKUs.objects.filter(id=id)
-        # print (obj)
-        return render(request, 'dashboard.html',{'skus': obj}) 
-    if 'search1' in request.GET:
-        id = request.GET['search1']
-        obj = SKUs.objects.filter(Description__contains= id)
-        print (obj)
-        return render(request, 'dashboard.html',{'skus': obj}) 
-    
-    skus = SKUs.objects.all()
-    return render(request, 'dashboard.html',{'skus': skus})
+    try:
+        skus = SKUs.objects.all()
+    except SKUs.DoesNotExist:
+        skus = get_object_or_404
+    # sku = {{'Description':'router microtic'},{'Type':'Gateway'},{'Quantity',10},{'Price': 3000}}
+    for sku in skus:
+        print(sku)
+    return render(request, 'dashboard.html', {'skus': skus})
+
 
 def Logout(request):
     if request.method == 'POST':
@@ -63,28 +49,20 @@ def Logout(request):
     return redirect('Login')
 
 
- 
 # Dashboard-date Module
 class dashboard(View):
 
     def post(self, request):
-        data = json.loads(request.body) 
+        data = json.loads(request.body)
         date = data['date']
         print(date)
         return JsonResponse({'date': data['date']})
 
 
-
-
-#Stock release module
+# Stock release module
 class CapexOut(View):
-     def post(self, request):
-        data = json.loads(request.body) 
+    def post(self, request):
+        data = json.loads(request.body)
         search = data['sku']
-        skus = SKUs.objects.filter(Description__contains= search).values()
+        skus = SKUs.objects.filter(Description__contains=search).values()
         return JsonResponse({'search': list(skus)})
-
-
-
- 
- 
