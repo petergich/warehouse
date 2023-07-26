58144a712839a71b11aacafa1b01681f1ebe8e47
petergich <petergich80@gmail.com>
2023-07-26 15:53:37 +0300

    'Petergich'

--- rubly/management_system/static/js/capexout.js
+++ rubly/management_system/static/js/capexout.js
@@ -1,4 +1,6 @@
 const searchField = document.querySelector(".search_input");
+const mytable=document.getElementById("mytable");
+display()
 console.log(searchField);
 searchField.addEventListener("keyup", (event) => {
   const search = event.target.value;
@@ -50,10 +52,14 @@ function myfunc1() {
   var url = "dashboard?search1=" + encodeURIComponent(searchField.value);
   window.location.href = url;
 }
-function myfunc2(id) {
+function myfunc2(id,des,max) {
   var amount=document.getElementById(id).value;
   if(amount<1){
     alert("Please enter a Valid amount");
+    return false;
+  }
+  else if(parseInt(amount)>parseInt(max)){
+    alert("The amount exceeds available amount");
     return false;
   }
 // Function to add elements to session storage
@@ -68,10 +74,11 @@ function myfunc2(id) {
   const dataArray = [];
 
   // Append the new value to the array
-  dataArray.push(id,amount);
+  dataArray.push(id,amount,des);
 
   // Convert the updated array back to a JSON string and store it in session storage
   sessionStorage.setItem(id, dataArray);
+  mytable.innerHTML='';
   display();
 
   // Function to get all data from session storage
@@ -79,20 +86,97 @@ function myfunc2(id) {
  // Return the object containing all session storage data
 }
  function display(){
-
   // Iterate over each key in sessionStorage
-  const mytable=document.getElementById("mytable")
   for (let i = 0; i < sessionStorage.length; i++) {
     const key = sessionStorage.key(i);
-    const value = sessionStorage.getItem(key);
-    mytable.innerHTML +="<tr id="+i+'></tr>';
-    for(let k=0;k<value.length;k+=2){
-      const myrow=document.getElementById(i);
-      if(k===0){
-      }
+    const array = sessionStorage.getItem(key);
+    const value=array.split(",");
+    var myrow=document.createElement('tr');
+    mytable.appendChild(myrow);
+    var des=value[2];
+    var d=document.createElement('td');
+    d.textContent = des;
+    myrow.appendChild(d);
+    var a=document.createElement('td');
+    a.textContent=value[1];
+    myrow.appendChild(a);
+    var b=document.createElement('td');
+    myrow.appendChild(b)
+    var bu=document.createElement('button');
+    bu.textContent="delete";
+    bu.addEventListener('click', function() {
+      var arg1 = key; // Replace this with your desired argument
+      myfunc3(arg1);
+    });
+    b.appendChild(bu);
+        }
     }
- }
- }
+    function myfunc3(key){
+      sessionStorage.removeItem(key);
+      mytable.innerHTML='';
+      display()
+    } 
+    function clearall(){
+      sessionStorage.clear();
+      mytable.innerHTML='';
+    }
+    function checkout(){
+      const myarray=[];
+      for(let i=0;i<sessionStorage.length;i++){
+        var key=sessionStorage.key(i);
+        var array=sessionStorage.getItem(key);
+        const value=array.split(",");
+        value.pop()
+        myarray.push(value[0],value[1]);
+      }
+      var capex=document.getElementById('capex').value;
+      if(capex!=''){
+      var url = 'order/?array='+encodeURIComponent(myarray)+'&capex=' + encodeURIComponent(capex);
+
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', url, true);
+      xhr.onload = function() {
+        if (xhr.status === 200) {
+          var options = JSON.parse(xhr.responseText);
+          console.log(options);
+          if(options.date==="Successfull") {
+            clearall();
+            var url = "dashboard" ;
+            window.location.href = url;
+          }
+          else if(options.date==="Capex not found"){
+            alert(options.date);
+          }         
+        }
+      };
+    
+      xhr.send();
+    }
+    else{
+     
+      var url = 'order/?array='+encodeURIComponent(myarray);
+
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', url, true);
+      xhr.onload = function() {
+        if (xhr.status === 200) {
+          var options = JSON.parse(xhr.responseText);
+          if(options.date==="Successfull") {
+            clearall();
+            var url = "dashboard" ;
+            window.location.href = url;
+          } 
+          else {
+            alert("An Error Occured")
+          }    
+        }
+      };
+    
+      xhr.send();
+    }
+  }
+  
+
 
 
 
--- rubly/management_system/views.py
+++ rubly/management_system/views.py
@@ -11,7 +11,9 @@ from django.views import View
 import json
 from django.contrib.auth.models import User
 from django.http import JsonResponse
-
+import os
+import time
+import datetime
 
 
 
@@ -61,6 +63,59 @@ def Logout(request):
         messages.success(request, 'You are now logged out')
         return redirect('Login')
     return redirect('Login')
+def selected(request):
+    # redirect('Dashboard')
+    if "array" in request.GET:
+        if 'capex'in request.GET:
+            capex=request.GET['capex']
+            string=request.GET['array']
+            array = string.split(',')
+            mycapex=Capex.objects.get(capex_no=capex)
+            if(mycapex):
+                for i in range(0,len(array),2):
+                    itemid=array[i]
+                    amount=array[i+1]
+                    instance=SKUs.objects.get(id=itemid)
+                    instance.Quantity=int(instance.Quantity)-int(amount)
+                    instance.save()
+                now = datetime.datetime.now()
+
+                # Convert the datetime object to a string
+                current_date = now.strftime('%Y_%m_%d_%H_%M_%S')  # Generate a date-only timestamp
+                directory = "transactions"
+                # Create the directory if it doesn't exist
+                if not os.path.exists(directory):
+                    os.makedirs(directory)
+                file_name = os.path.join("transactions", f"_{capex}_{current_date}.json")  # Create a file path within the "data" directory
+
+                with open(file_name, 'w') as file:
+                    print("Success")
+                    json.dump(string, file)
+                return JsonResponse({'date': "Successfull"})
+            else:
+                return JsonResponse({'date': "Capex not found"})
+        elif 'capex' not in request.GET:
+            string=request.GET['array']
+            array = string.split(',')
+            for i in range(0,len(array),2):
+                    itemid=array[i]
+                    amount=array[i+1]
+                    instance=SKUs.objects.get(id=itemid)
+                    instance.Quantity=instance.Quantity-int(amount)
+                    instance.save()
+            now = datetime.datetime.now()
+            current_date = now.strftime('%Y_%m_%d_%H_%M_%S')  # Generate a date-only timestamp
+            directory = "transactions"
+            # Create the directory if it doesn't exist
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            file_name = os.path.join("transactions", f"_Null_{current_date}.json")  # Create a file path within the "data" directory
+
+            with open(file_name, 'w') as file:
+                print("Success")
+                json.dump(string, file)
+            return JsonResponse({'date': "Successfull"})
+    
 
 
  
@@ -83,7 +138,8 @@ class CapexOut(View):
         search = data['sku']
         skus = SKUs.objects.filter(Description__contains= search).values()
         return JsonResponse({'search': list(skus)})
-
+ 
+    
 
 
  
